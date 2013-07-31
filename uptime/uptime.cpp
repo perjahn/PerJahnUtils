eeb2da2711b286fc4d67b07be589f787c93ddf16
Per <per.jahn@wipcore.se>
2013-07-31 06:06:25 +0200

    uptime

--- uptime/uptime.cpp
+++ uptime/uptime.cpp
@@ -4,10 +4,11 @@
 void main(void)
 {
 	unsigned days, hours, minutes, seconds;
-	days = GetTickCount()/1000/3600/24;
-	hours = GetTickCount()/1000/3600-days*24;
-	minutes = GetTickCount()/1000/60-hours*60-days*24*60;
-	seconds = GetTickCount()/1000-minutes*60-hours*3600-days*24*3600;
+	DWORD uptime = GetTickCount();
+	days = uptime/1000/3600/24;
+	hours = uptime/1000/3600-days*24;
+	minutes = uptime/1000/60-hours*60-days*24*60;
+	seconds = uptime/1000-minutes*60-hours*3600-days*24*3600;
 
 	printf("%u days, %uh, %um, %us\n", days, hours, minutes, seconds);
 }
