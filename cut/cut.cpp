54332d2e11ea9cd93f366a93438b1e60d61cf5c6
Per <per.jahn@wipcore.se>
2013-11-13 09:47:43 +0100

    cut: files >4gb

--- cut/cut.cpp
+++ cut/cut.cpp
@@ -1,6 +1,7 @@
 /*
 2.1 Fixed bug: could write larger outfile than infile, appended junk.
 2.2 Added return value.
+2.3 Fixed bug, again: could write larger outfile than infile, appended junk.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,41 +11,52 @@ int main(int argc, char *argv[])
 	if(argc!=5)
 	{
 		printf(
-			"cut 2.2\n"
+			"cut 2.3\n"
 			"\n"
 			"Usage: cut <infile> <outfile> <start> <length>\n");
 		return 0;
 	}
 
-	long start, length;
+	long long start, length;
 
-	start = atol(argv[3]);
-	length = atol(argv[4]);
+	start = _atoi64(argv[3]);
+	length = _atoi64(argv[4]);
 
-	unsigned char *buf = new unsigned char[length];
-	if(!buf)
-	{
-		printf("Out of memory (%u bytes).\n", length);
-		return 0;
-	}
 
 	FILE *fh;
 
 	if(!(fh = fopen(argv[1], "rb")))
 	{
-		delete[] buf;
-		printf("Couldn't open infile (%s).\n", argv[1]);
+		printf("Couldn't open infile: '%s'\n", argv[1]);
+		return 1;
+	}
+
+	_fseeki64(fh, 0, SEEK_END);
+	long long filesize = _ftelli64(fh);
+
+	if (start+length > filesize)
+	{
+		length = filesize-start;
+	}
+
+	unsigned char *buf = new unsigned char[length];
+	if(!buf)
+	{
+		printf("Out of memory: %u bytes.\n", length);
 		return 0;
 	}
 
-	fseek(fh, start, SEEK_SET);
+
+	printf("Copying %llu bytes from '%s' to '%s'.\n", length, argv[1], argv[2]);
+
+	_fseeki64(fh, start, SEEK_SET);
 	fread(buf, length, 1, fh);
 	fclose(fh);
 
 	if(!(fh = fopen(argv[2], "wb")))
 	{
 		delete[] buf;
-		printf("Couldn't open outfile (%s).\n", argv[2]);
+		printf("Couldn't open outfile: '%s'\n", argv[2]);
 		return 0;
 	}
 
