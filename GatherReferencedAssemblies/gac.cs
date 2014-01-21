28c0197063ad550cea3ba1fa94e25b7af889df21
Per <perjahn@gmail.com>
2014-01-21 08:47:07 +0100

    Support for gathering of referenced dll files.

---
+++ GatherReferencedAssemblies/gac.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.GACManagedAccess;
+
+namespace GatherReferencedAssemblies
+{
+	class gac
+	{
+		static List<string> SystemAssemblies = null;
+
+		private static void InitIsSystemAssembly()
+		{
+			SystemAssemblies = new List<string>();
+
+			AssemblyCacheEnum asmEnum = new AssemblyCacheEnum(null);
+			string nextAsm;
+			while ((nextAsm = asmEnum.GetNextAssembly()) != null)
+			{
+				SystemAssemblies.Add(nextAsm.Split(',')[0]);
+			}
+
+			int count1 = SystemAssemblies.Count();
+			SystemAssemblies = SystemAssemblies.Distinct().OrderBy(a => a).ToList();
+			int count2 = SystemAssemblies.Count();
+		}
+
+		public static bool IsSystemAssembly(string assemblyname, bool ignoreCase)
+		{
+			if (SystemAssemblies == null)
+				InitIsSystemAssembly();
+
+			if (SystemAssemblies.Any(a => string.Compare(assemblyname, a, ignoreCase) == 0))
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
