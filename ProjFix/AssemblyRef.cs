280b8fecbe302af6cf7201a708976d90c72a6a66
Per <perjahn@gmail.com>
2014-01-21 11:13:04 +0100

    Restructured classes.

---
+++ ProjFix/AssemblyRef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjFix
+{
+	// Corresponds to a <Reference> tag in project file.
+	class AssemblyRef
+	{
+		public string include { get; set; }
+		public string shortinclude { get; set; }  // Assembly name
+		public string name { get; set; }
+		public string hintpath { get; set; }
+		public bool? copylocal { get; set; }  // Xml tag name=Private
+
+		// These 3 lists are used when loading, before validating/fixing, and compacting them to single items.
+		public List<string> names { get; set; }
+		public List<string> hintpaths { get; set; }
+		public List<string> copylocals { get; set; }
+	}
+}
---
+++ ProjFix/ProjectRef.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjFix
+{
+	// Corresponds to a <ProjectReference> tag in project file.
+	class ProjectRef
+	{
+		public string include { get; set; }
+		public string shortinclude { get; set; }  // Project file name
+		public string name { get; set; }
+		public string project { get; set; }
+		public string package { get; set; }
+
+		// These 3 lists are used when loading, before validating/fixing, and compacting them to single items.
+		public List<string> names { get; set; }
+		public List<string> projects { get; set; }
+		public List<string> packages { get; set; }
+	}
+}
