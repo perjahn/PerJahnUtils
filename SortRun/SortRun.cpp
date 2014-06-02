d2d509c6c4e98d8a8b3714c1e2c00e333a28ea4f
Per <perjahn@gmail.com>
2014-06-02 12:00:13 +0200

    Added tool: SortRun

---
+++ SortRun/SortRun.cpp
@@ -0,0 +1,59 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+void main(int argc, char *argv[])
+{
+	char *usage =
+		"SortRun 0.001 gamma"
+		"\n"
+		"Usage: SortRun <file1> <file2> <command>\n"
+		"\n"
+		"Example: sortrun myfile1 myfile2 diff\n";
+
+	if (argc != 4)
+	{
+		printf(usage);
+		return;
+	}
+
+	if (strlen(argv[1]) > 1000)
+	{
+		printf("Filename 1 too long.");
+		return;
+	}
+	if (strlen(argv[2]) > 1000)
+	{
+		printf("Filename 2 too long.");
+		return;
+	}
+	if (strlen(argv[3]) > 1000)
+	{
+		printf("Command too long.");
+		return;
+	}
+
+	char tempdir[1000];
+
+	if (!GetTempPath(500, tempdir))
+	{
+		printf("Couldn't get temp dir (%u).\n", GetLastError());
+		return;
+	}
+
+	char command[5000];
+
+	sprintf(command, "sort \"%s\" > \"%ssortfile1.txt\"", argv[1], tempdir);
+	//printf(">>>%s<<<\n", command);
+	system(command);
+
+	sprintf(command, "sort \"%s\" > \"%ssortfile2.txt\"", argv[2], tempdir);
+	//printf(">>>%s<<<\n", command);
+	system(command);
+
+	sprintf(command, "%s \"%ssortfile1.txt\" \"%ssortfile2.txt\"", argv[3], tempdir, tempdir);
+	//printf(">>>%s<<<\n", command);
+	system(command);
+
+	return;
+}
