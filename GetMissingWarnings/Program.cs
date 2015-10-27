013badced1920e56e1bc6d3f19749604dc7e0e6b
Per Jahn <perjahn@gmail.com>
2015-10-27 07:58:02 +0100

    More comprehensive logging & features.

--- GetMissingWarnings/Program.cs
+++ GetMissingWarnings/Program.cs
@@ -10,11 +10,24 @@ namespace GetMissingWarnings
 {
     class Program
     {
+        private static int _missingfiles = 0;
+        private static int _totalmissing = 0;
+
         static void Main(string[] args)
         {
+            bool allenabled = args.Contains("-a");
+            args = args.Where(a => a != "-a").ToArray();
+
+            string[] exclude = args.Where(a => a.StartsWith("-e")).SelectMany(a => a.Substring(2).Split(',')).ToArray();
+            args = args.Where(a => !a.StartsWith("-e")).ToArray();
+
             if (args.Length != 1)
             {
-                Console.WriteLine("Usage: GetMissingWarnings <path>");
+                Console.WriteLine(
+@"Usage: GetMissingWarnings [-a] [-eProj1,Proj2] <path>
+
+-a:  Display all (non-conditional) sections missing TreatWarningsAsErrors element.
+-e:  Exclude projects.");
                 return;
             }
 
@@ -32,13 +45,34 @@ namespace GetMissingWarnings
                 return;
             }
 
-            foreach (string file in files)
+            files = files
+                .Select(f => f.StartsWith(@".\") ? f.Substring(2) : f)
+                .ToArray();
+
+            string[] filteredfiles = files.Where(f => !exclude.Any(f.Contains)).ToArray();
+
+            if (filteredfiles.Length < files.Length)
             {
-                LoadProject(file);
+                WriteLineColor("Excluding: " + (files.Length - filteredfiles.Length) + " files:", ConsoleColor.Yellow);
+                foreach (string file in files.Where(f => !filteredfiles.Contains(f)))
+                {
+                    WriteLineColor("  '" + file + "'", ConsoleColor.Yellow);
+                }
             }
+
+
+            foreach (string file in filteredfiles)
+            {
+                LoadProject(file, allenabled);
+            }
+
+
+            Console.WriteLine();
+            WriteLineColor("Missing files: " + _missingfiles, ConsoleColor.Magenta);
+            WriteLineColor("Missing groups: " + _totalmissing, ConsoleColor.Magenta);
         }
 
-        static void LoadProject(string projectfile)
+        static void LoadProject(string projectfile, bool allenabled)
         {
             XDocument xdoc;
             XNamespace ns;
@@ -73,23 +107,63 @@ namespace GetMissingWarnings
 
             // Is there any config that is missing warnings-as-errors, but has siblings which has warnings-as-errors?
 
-            var elements = xdoc
-                .Elements(ns + "Project")
-                .Elements(ns + "PropertyGroup")
-                .Where(el => !el.Elements(ns + "TreatWarningsAsErrors").Any() && el.Attributes().Any() &&
-                    el.Parent.Elements(ns + "PropertyGroup").Where(el2 => el2.Elements(ns + "TreatWarningsAsErrors").Any() && el2.Attributes().Any()).Any());
-
-            if (elements.Any())
+            if (allenabled)
             {
-                var color = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("********** '" + projectfile + "' " + elements.Count() + " **********");
-                Console.ForegroundColor = color;
+                var elements = xdoc
+                    .Elements(ns + "Project")
+                    .Elements(ns + "PropertyGroup")
+                    .Where(el => !el.Elements(ns + "TreatWarningsAsErrors").Any() && el.Attributes().Any());
 
-                Console.WriteLine(string.Join(Environment.NewLine, elements.Select(el => el.ToString())));
+                if (elements.Any())
+                {
+                    WriteLineColor("********** '" + projectfile + "' " + elements.Count() + " **********", ConsoleColor.Cyan);
+
+                    Console.WriteLine(string.Join(Environment.NewLine, elements.Select(el => el.ToString())));
+                }
+
+                _totalmissing += elements.Count();
+                if (elements.Count() > 0)
+                {
+                    _missingfiles++;
+                }
+            }
+            else
+            {
+                var elements = xdoc
+                    .Elements(ns + "Project")
+                    .Elements(ns + "PropertyGroup")
+                    .Where(el => !el.Elements(ns + "TreatWarningsAsErrors").Any() && el.Attributes().Any() &&
+                        el.Parent.Elements(ns + "PropertyGroup").Where(el2 => el2.Elements(ns + "TreatWarningsAsErrors").Any() && el2.Attributes().Any()).Any());
+
+                if (elements.Any())
+                {
+                    WriteLineColor("********** '" + projectfile + "' " + elements.Count() + " **********", ConsoleColor.Cyan);
+
+                    Console.WriteLine(string.Join(Environment.NewLine, elements.Select(el => el.ToString())));
+                }
+
+                _totalmissing += elements.Count();
+                if (elements.Count() > 0)
+                {
+                    _missingfiles++;
+                }
             }
 
             return;
+        }
+
+        private static void WriteLineColor(string message, ConsoleColor color)
+        {
+            var oldcolor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            try
+            {
+                Console.WriteLine(message);
+            }
+            finally
+            {
+                Console.ForegroundColor = oldcolor;
+            }
         }
     }
 }
