9dd4c58c18e56cbad939020ff3b23024188a19f1
Per Jahn <perjahn@gmail.com>
2015-12-06 20:52:15 +0100

    Fixed assembly title.

--- GatherOutputAssemblies/Properties/AssemblyInfo.cs
+++ GatherOutputAssemblies/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProjFix")]
+[assembly: AssemblyTitle("GatherOutputAssemblies")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ProjFix")]
+[assembly: AssemblyProduct("GatherOutputAssemblies")]
 [assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
