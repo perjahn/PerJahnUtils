50306ae1dc01f1f3469602ce739c5bc17a91451a
Per Jahn <perjahn@gmail.com>
2015-12-06 01:49:07 +0100

    Updated gather script.

--- postbuild.ps1
+++ postbuild.ps1
@@ -5,22 +5,53 @@ $ErrorActionPreference = "Stop"
 
 function Main()
 {
-    Move-PublicArtifacts
+    if ($env:artifactpath)
+    {
+        [string] $artifactpath = $env:artifactpath
+    }
+    else
+    {
+        [string] $artifactpath = "_Artifacts"
+    }
+
+    if ($env:buildconfig)
+    {
+        [string] $buildconfig = $env:buildconfig
+    }
+    else
+    {
+        [string] $buildconfig = "Release"
+    }
+
+    GatherFiles $artifactpath $buildconfig
 }
 
-function Move-PublicArtifacts([string] $buildfile)
+function GatherFiles([string] $artifactpath, [string] $buildconfig)
 {
-    [string] $source = "\\wipweb01.wipcore.se\downloads\PerJ\Users\Users_PerJ\PerJahnUtils.zip"
-    [string] $target = "\\wipweb01.wipcore.se\downloads\PerJ\PerJahnUtils.zip"
-    if (Test-Path $target)
-    {
-        del $target
-    }
-    move $source $target
+    Write-Host ("Artifact path: '" + $artifactpath + "'")
+    Write-Host ("Build config: '" + $buildconfig + "'")
 
-    [string] $folder = Split-Path (Split-Path $source)
-    Write-Host ("Removing folder: '" + $folder + "'")
-    rd $folder -Recurse
+    [string] $toolpath = "GatherOutputAssemblies\bin\Release\GatherOutputAssemblies.exe"
+
+    if (!(Test-Path $toolpath))
+    {
+        Write-Host ("File not found: '" + $toolpath + "'")
+        return 1
+    }
+
+    if (Test-Path $artifactpath)
+    {
+        write-Host ("Removing artifact directory: '" + $artifactpath + "'")
+        rd -Recurse $artifactpath
+    }
+
+    write-Host ("Creating artifact directory: '" + $artifactpath + "'")
+    md $artifactpath | Out-Null
+
+    dir -r -i *.sln | ? { !($_.Attributes -band [IO.FileAttributes]::Directory) } | % {
+        [string] $solutionfile = $_
+        &$toolpath $solutionfile $buildconfig $artifactpath
+    }
 }
 
 Main
