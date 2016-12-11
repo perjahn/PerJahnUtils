8aadb5576aff9c6e25148f3546c796182614f975
Per Jahn <perjahn@gmail.com>
2016-12-11 04:57:47 +0100

    Added XmlViewer

---
+++ xmlviewer/XmlViewer.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace xmlviewer
+{
+    partial class XmlViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XmlViewer));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.HideSelection = false;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(984, 1961);
+            this.treeView1.TabIndex = 0;
+            // 
+            // XmlViewer
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 1961);
+            this.Controls.Add(this.treeView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "XmlViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xml Viewer";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.XmlViewer_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.XmlViewer_DragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
---
+++ xmlviewer/XmlViewer.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace xmlviewer
+{
+    public partial class XmlViewer : Form
+    {
+        public XmlViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void XmlViewer_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                if (files.Length == 1 && File.Exists(files[0]))
+                {
+                    e.Effect = DragDropEffects.Copy;
+                }
+                else
+                {
+                    e.Effect = DragDropEffects.None;
+                }
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void XmlViewer_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])(e.Data.GetData(DataFormats.FileDrop));
+                if (files.Length == 1 && File.Exists(files[0]))
+                {
+                    XDocument xdoc;
+                    try
+                    {
+                        xdoc = XDocument.Load(files[0]);
+                    }
+                    catch (Exception ex) when (ex is IOException || ex is UnauthorizedAccessException || ex is XmlException)
+                    {
+                        this.Text = files[0] + ": " + ex.Message;
+                        return;
+                    }
+
+                    treeView1.Nodes.Clear();
+
+                    this.Text = files[0] + ": " + xdoc.Descendants().Count() + " elements.";
+
+                    AddNode(treeView1.Nodes, xdoc.Root);
+                }
+            }
+        }
+
+        private void AddNode(TreeNodeCollection parentTreeNodes, XElement element)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string attrstring = string.Join(" ", element.Attributes().Select(a => a.Name + "=\"" + a.Value + "\""));
+            if (attrstring != string.Empty)
+            {
+                sb.Append(" " + attrstring);
+            }
+
+            string innertext = element.Value;
+            if (innertext != string.Empty)
+            {
+                sb.Append(" Value=\"" + innertext + "\"");
+            }
+
+            TreeNode treenode = new TreeNode();
+            treenode.Text = element.Name.LocalName + sb.ToString();
+            parentTreeNodes.Add(treenode);
+
+            foreach (XElement child in element.Descendants())
+            {
+                AddNode(treenode.Nodes, child);
+            }
+
+            treenode.Expand();
+        }
+    }
+}
