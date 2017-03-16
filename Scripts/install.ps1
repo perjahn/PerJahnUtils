61432ac3b064553d6378c85df65c9e4b3490a2d3
Per <perjahn@gmail.com>
2017-03-17 00:58:57 +0100

    Added putty

--- Scripts/install.ps1
+++ Scripts/install.ps1
@@ -1,2 +1,2 @@
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-powershell "choco install googlechrome notepadplusplus '7zip' 'cpu-z' -y"
+powershell "choco install googlechrome notepadplusplus '7zip' 'cpu-z' putty -y"
