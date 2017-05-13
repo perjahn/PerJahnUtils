90c42c20944a6f720a213aa8542e3e8fc23f33c8
Per Jahn <perjahn@gmail.com>
2017-05-13 05:07:46 +0200

    Added base64 encode/decode tools.

---
+++ Base64Decode/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64Decode
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Base64Decode <string>");
+                return 1;
+            }
+
+            //string text = File.ReadAllText(args[0]);
+            byte[] bytes = Convert.FromBase64String(args[0]);
+            foreach (byte b in bytes)
+            {
+                char c = (char)b;
+                Console.Write(c);
+            }
+            Console.WriteLine();
+
+            return 0;
+        }
+    }
+}
---
+++ Base64Encode/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base64Encode
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Base64Encode <filename>");
+                return 1;
+            }
+
+            byte[] bytes = File.ReadAllBytes(args[0]);
+            string encodedstring = Convert.ToBase64String(bytes);
+            Console.WriteLine(encodedstring);
+
+            return 0;
+        }
+    }
+}
