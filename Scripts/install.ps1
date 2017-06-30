4b33cfb7a7ffaafc67a68ceef208368d2da409fc
Per Jahn <perjahn@gmail.com>
2017-07-01 01:03:07 +0200

    Added some tools.

--- Scripts/install.ps1
+++ Scripts/install.ps1
@@ -1,2 +1,2 @@
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
-powershell "choco install googlechrome notepadplusplus '7zip' 'cpu-z' putty -y"
+powershell "choco install googlechrome notepadplusplus '7zip' 'cpu-z' putty procmon procexp autoruns -y"
