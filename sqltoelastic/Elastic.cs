89c43d7077c2c0f6cc999c9708f5a340f078feff
Per Jahn <perjahn@gmail.com>
2017-11-28 09:49:51 +0100

    Bulk insert.

--- sqltoelastic/Elastic.cs
+++ sqltoelastic/Elastic.cs
@@ -14,54 +14,75 @@ namespace sqltoelastic
         public void PutIntoIndex(string serverurl, string username, string password, string indexname,
             string typename, string timestampfield, string idprefix, string idfield, JObject[] jsonrows)
         {
+            string bulkdata;
+
             using (WebClient client = new WebClient())
             {
-                if (username != null && password != null)
-                {
-                    string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
-                    client.Headers[HttpRequestHeader.Authorization] = $"Basic {credentials}";
-                }
-
                 int rownum = 0;
+
+                string address = $"{serverurl}/_bulk";
+
+                StringBuilder sb = new StringBuilder();
 
                 foreach (JObject jsonrow in jsonrows)
                 {
-                    client.Headers["Content-Type"] = "application/json";
-                    client.Headers["Accept"] = "application/json";
-
                     jsonrow["@timestamp"] = jsonrow[timestampfield];
                     DateTime created = jsonrow[timestampfield].Value<DateTime>();
 
                     string datestring = created.ToString("yyyy.MM");
                     string dateindexname = $"{indexname}-{datestring}";
-
                     string id = $"{idprefix}{jsonrow[idfield].Value<string>()}";
 
-                    string address = $"{serverurl}/{dateindexname}/{typename}/{id}";
+                    string metadata = "{ \"index\": { \"_index\": \"" + dateindexname + "\", \"_type\": \"" + typename + "\", \"_id\": \"" + id + "\" } }";
+                    sb.AppendLine(metadata);
 
-                    string data = jsonrow.ToString();
-                    string result = string.Empty;
+                    string rowdata = jsonrow.ToString().Replace("\r", string.Empty).Replace("\n", string.Empty);
+                    sb.AppendLine(rowdata);
 
-                    try
-                    {
-                        client.Encoding = Encoding.UTF8;
-                        result = client.UploadString(address, "PUT", data);
-                    }
-                    catch (WebException ex)
-                    {
-                        Log($"Put '{address}': >>>{data}<<<");
-                        Log($"Result: >>>{result}<<<");
-                        Log($"Exception: >>>{ex.ToString()}<<<");
-                    }
+                    rownum++;
 
                     if (rownum % 1000 == 0)
                     {
                         Log($"Importing rows: {rownum}");
+
+                        bulkdata = sb.ToString();
+                        ImportRows(client, address, username, password, bulkdata);
+                        sb = new StringBuilder();
                     }
-                    rownum++;
+                }
+
+                bulkdata = sb.ToString();
+                if (bulkdata.Length > 0)
+                {
+                    Log($"Importing rows: {rownum}");
+                    ImportRows(client, address, username, password, bulkdata);
                 }
 
                 Log("Done!");
+            }
+        }
+
+        private void ImportRows(WebClient client, string address, string username, string password, string bulkdata)
+        {
+            if (username != null && password != null)
+            {
+                string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{username}:{password}"));
+                client.Headers[HttpRequestHeader.Authorization] = $"Basic {credentials}";
+            }
+            client.Headers["Content-Type"] = "application/x-ndjson";
+            client.Headers["Accept"] = "application/json";
+            client.Encoding = Encoding.UTF8;
+
+            string result = string.Empty;
+            try
+            {
+                result = client.UploadString(address, bulkdata);
+            }
+            catch (WebException ex)
+            {
+                Log($"Put '{address}': >>>{bulkdata}<<<");
+                Log($"Result: >>>{result}<<<");
+                Log($"Exception: >>>{ex.ToString()}<<<");
             }
         }
 
