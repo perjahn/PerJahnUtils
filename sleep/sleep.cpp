478a91b8ac275626910177feec8cfad6f22e4be6
Per Jahn <perjahn@gmail.com>
2019-01-23 21:13:29 +0100

    Added sleep tool.

---
+++ sleep/sleep.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: sleep.exe <milliseconds>\n");
+		return 1;
+	}
+	Sleep(atoi(argv[1]));
+	return 0;
+}
