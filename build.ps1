37db3571c10dc425d4f280e2c5d3547a42677752
Per Jahn <perjahn@gmail.com>
2019-01-23 21:32:16 +0100

    Locate msbuild.exe.

--- build.ps1
+++ build.ps1
@@ -5,11 +5,42 @@ $ErrorActionPreference = "Stop"
 
 function Main()
 {
+    [Diagnostics.Stopwatch] $watch = [Diagnostics.Stopwatch]::StartNew()
+
+    [string] $msbuild = Locate-MsBuildBinary
+
     .\prebuild.ps1
 
-    msbuild "all.build" "/p:Configuration=Release"
+    &$msbuild "all.build" "/p:Configuration=Release"
 
     .\postbuild.ps1
+
+    Write-Host ("Done: " + $watch.Elapsed)
+}
+
+function Locate-MsBuildBinary()
+{
+    if (Get-Command "msbuild" -ErrorAction SilentlyContinue)
+    {
+        return "msbuild"
+    }
+    else
+    {
+        [string] $vspath = "C:\Program Files (x86)\Microsoft Visual Studio"
+        Write-Host ("Searching for msbuild.exe...")
+        $binaries = @(dir -Recurse $vspath -Include "msbuild.exe" | ? { (Split-Path (Split-Path $_.FullName) -Leaf) -eq "amd64" })
+        if ($binaries.Count -ge 1)
+        {
+            [string] $binary = $binaries | select -First 1 | % { $_.FullName }
+            Write-Host ("Using: '" + $binary + "'")
+            return $binary
+        }
+        else
+        {
+            Write-Host ("Couldn't find msbuild.exe") -f Red
+            exit 1
+        }
+    }
 }
 
 Main
